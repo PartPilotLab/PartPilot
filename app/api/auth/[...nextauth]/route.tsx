7eb56c10c13b3d925ae5b52c0f305b2aa0371176
LenniM <mikuszewski.lenni@gmail.com>
2024-04-02 15:42:33 +0200

    TypeError (reading GET) fix ✅

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,28 +1,36 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import {compare} from "bcrypt";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
 
-export const { handlers: { GET, POST }, auth } = NextAuth({
-	providers: [
-		CredentialsProvider({
-			name: 'Credentials',
-			credentials: {
-				password: {},
-				email: {}
-			},
-			async authorize(credentials, req) {
-				const user = await prisma.user.findUnique({where: {email: credentials.email}})
-				if (!user) return null
-				const passwordCorrect = await compare(credentials.password, user.password)
-				if (passwordCorrect) return {
-					id: user.id,
-					name: user.name,
-					email: user.email,
-					image: user.image
-				}
-				return null
-			}
-		})
-	],
-	session: {strategy: "jwt"}
-})
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        password: {},
+        email: {},
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+        const passwordCorrect = await compare(
+          credentials.password,
+          user.password
+        );
+        if (passwordCorrect)
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          };
+        return null;
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+});
+
+export { handler as GET, handler as POST };
