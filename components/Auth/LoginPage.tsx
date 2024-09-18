0bcacfcd51fd1b490011a4483c4f9bc8800d532c
Berthose Fin <berthosefin@gmail.com>
2024-09-18 11:13:22 +0300

    Refresh page after log in and sign up

--- components/Auth/LoginPage.tsx
+++ components/Auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Button, TextInput } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
@@ -8,44 +8,52 @@ import { useRouter } from "next/navigation";
 import AuthFormContainer from "./AuthFormContainer";
 
 export default function LoginPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget)
-        await signIn('credentials', {
-            email: formData.get("email"),
-            password: formData.get("password"),
-            redirect: false
-        }).then(({ ok }) => {
-            //Check if the login was successful
-            if (ok) {
-                notifications.show({ title: "Success", message: "Logged in successfully!", color: "green" })
-                router.push("/");
-            } else {
-                notifications.show({ title: "Error", message: "You have entered an invalid email or password.", color: "red" })
-            }
-        })
-    }
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    }).then(({ ok }) => {
+      //Check if the login was successful
+      if (ok) {
+        notifications.show({
+          title: "Success",
+          message: "Logged in successfully!",
+          color: "green",
+        });
+        router.push("/");
+        router.refresh();
+      } else {
+        notifications.show({
+          title: "Error",
+          message: "You have entered an invalid email or password.",
+          color: "red",
+        });
+      }
+    });
+  };
 
-    return (
-        <AuthFormContainer handleSubmit={handleLogin}>
-            <TextInput
-                label="Email"
-                name='email'
-                placeholder="Email"
-                type="email"
-                required
-            />
-            <TextInput
-                name='password'
-                label="Password"
-                placeholder="Password"
-                type="password"
-                required
-            />
-            <Button type="submit">Login</Button>
-
-        </AuthFormContainer>
-    )
+  return (
+    <AuthFormContainer handleSubmit={handleLogin}>
+      <TextInput
+        label="Email"
+        name="email"
+        placeholder="Email"
+        type="email"
+        required
+      />
+      <TextInput
+        name="password"
+        label="Password"
+        placeholder="Password"
+        type="password"
+        required
+      />
+      <Button type="submit">Login</Button>
+    </AuthFormContainer>
+  );
 }
