6a9875408a98312e488cb833cca572e98c2d99ef
Berthose Fin <berthosefin@gmail.com>
2024-09-18 12:13:46 +0300

    NavFooter always at the bottom of the page

--- components/PageLayout/PageLayout.tsx
+++ components/PageLayout/PageLayout.tsx
@@ -1,66 +1,82 @@
-'use client'
+"use client";
 
-import {ReactNode} from "react";
-import {Notifications} from "@mantine/notifications";
-import NavHeader from "@/lib/components/NavHeader";
+import {
+  Settings,
+  SettingsConsumer,
+  SettingsProvider,
+} from "@/components/SettingsProvider";
 import NavFooter from "@/lib/components/NavFooter";
-import {createTheme, MantineProvider} from "@mantine/core";
+import NavHeader from "@/lib/components/NavHeader";
+import { createTheme, Flex, MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import localFont from "next/font/local";
-import {SettingsProvider, SettingsConsumer, Settings} from "@/components/SettingsProvider";
-import {SessionProvider} from "next-auth/react";
-import {Session} from "next-auth";
+import { ReactNode } from "react";
 
 const myFont = localFont({
-	src: "../../public/fonts/Montserrat-Regular.woff2",
+  src: "../../public/fonts/Montserrat-Regular.woff2",
 });
 
 const theme = createTheme({
-	fontFamily: myFont.style.fontFamily,
-	primaryColor: "light",
-	colors: {
-		"light": [
-			"#e8fbfe",
-			"#d9f1f6",
-			"#b3e1ea",
-			"#89d0df",
-			"#69c2d5",
-			"#55bacf",
-			"#47b5cc",
-			"#369fb5",
-			"#278ea2",
-			"#007b8f"
-		]
-	},
+  fontFamily: myFont.style.fontFamily,
+  primaryColor: "light",
+  colors: {
+    light: [
+      "#e8fbfe",
+      "#d9f1f6",
+      "#b3e1ea",
+      "#89d0df",
+      "#69c2d5",
+      "#55bacf",
+      "#47b5cc",
+      "#369fb5",
+      "#278ea2",
+      "#007b8f",
+    ],
+  },
 });
 
 type Props = {
-	children: ReactNode,
-	session: Session
-}
+  children: ReactNode;
+  session: Session;
+};
 
-export default function PageLayout({children, session}: Props) {
-	return (
-		<MantineProvider theme={theme}>
-			<Notifications/>
-			<SessionProvider session={session}>
-			<SettingsProvider>
-				<SettingsConsumer>
-					{({initialized}: Settings) => {
-						return initialized && (
-							<section style={{padding: 0, margin: 0}}>
-								<nav style={{height: "60px"}}>
-									<NavHeader/>
-								</nav>
-								<main>{children}</main>
-								<footer>
-									<NavFooter/>
-								</footer>
-							</section>
-						)
-					}}
-				</SettingsConsumer>
-			</SettingsProvider>
-			</SessionProvider>
-		</MantineProvider>
-	)
+export default function PageLayout({ children, session }: Props) {
+  return (
+    <MantineProvider theme={theme}>
+      <Notifications />
+      <SessionProvider session={session}>
+        <SettingsProvider>
+          <SettingsConsumer>
+            {({ initialized }: Settings) => {
+              return (
+                initialized && (
+                  <section
+                    style={{
+                      padding: 0,
+                      margin: 0,
+                      minHeight: "100vh",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <nav style={{ height: "60px" }}>
+                      <NavHeader />
+                    </nav>
+                    <Flex direction="column" style={{ flex: 1 }}>
+                      <main style={{ flex: 1 }}>{children}</main>
+                      <footer>
+                        <NavFooter />
+                      </footer>
+                    </Flex>
+                  </section>
+                )
+              );
+            }}
+          </SettingsConsumer>
+        </SettingsProvider>
+      </SessionProvider>
+    </MantineProvider>
+  );
 }
