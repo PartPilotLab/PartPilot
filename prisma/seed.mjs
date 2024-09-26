599582f69a7c8dae2156dbd86f0d4eee601a7db4
Berthose Fin <berthosefin@gmail.com>
2024-09-26 08:15:27 +0300

    Update schema to set user relation nullable

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,46 +1,56 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("password123", 10);
-
-  await prisma.user.upsert({
-    where: { email: "user@example.com" },
+  await prisma.parts.upsert({
+    where: { productCode: "C1591" },
     update: {
-      name: "John Doe",
-      password: hashedPassword,
+      productModel: "CL10B104KB8NNNC",
+      quantity: 2,
+      capacitance: 1,
+      prices: [],
     },
     create: {
-      email: "user@example.com",
-      name: "John Doe",
-      password: hashedPassword,
-      Parts: {
-        create: [
-          {
-            productCode: "C1591",
-            productModel: "CL10B104KB8NNNC",
-            quantity: 2,
-            capacitance: 1,
-            prices: [],
-          },
-          {
-            productCode: "C154120",
-            productModel: "SDFL2012T150KTF",
-            quantity: 20,
-            capacitance: 1,
-            prices: [],
-          },
-          {
-            productCode: "C29538",
-            productModel: "X322530MSB4SI",
-            quantity: 5,
-            capacitance: 1,
-            prices: [],
-          },
-        ],
-      },
+      productCode: "C1591",
+      productModel: "CL10B104KB8NNNC",
+      quantity: 2,
+      capacitance: 1,
+      prices: [],
+    },
+  });
+
+  await prisma.parts.upsert({
+    where: { productCode: "C154120" },
+    update: {
+      productModel: "SDFL2012T150KTF",
+      quantity: 20,
+      capacitance: 1,
+      prices: [],
+    },
+    create: {
+      productCode: "C154120",
+      productModel: "SDFL2012T150KTF",
+      quantity: 20,
+      capacitance: 1,
+      prices: [],
+    },
+  });
+
+  await prisma.parts.upsert({
+    where: { productCode: "C29538" },
+    update: {
+      productModel: "X322530MSB4SI",
+      quantity: 5,
+      capacitance: 1,
+      prices: [],
+    },
+    create: {
+      productCode: "C29538",
+      productModel: "X322530MSB4SI",
+      quantity: 5,
+      capacitance: 1,
+      prices: [],
     },
   });
 }
